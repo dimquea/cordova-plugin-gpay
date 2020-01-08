5e41895eb46c89d66d0d5cb6169759e79fd1b5be
Srini-max <54628670+Srini-max@users.noreply.github.com>
2020-01-08 16:23:34 +0400

    Update GooglePay.java

--- src/android/GooglePay.java
+++ src/android/GooglePay.java
@@ -1,4 +1,4 @@
-package com.cordova.plugin;
+package GooglePay;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -7,11 +7,12 @@ import android.util.Log;
 
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.identity.intents.model.UserAddress;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.wallet.AutoResolveHelper;
+import com.google.android.gms.wallet.CardInfo;
 import com.google.android.gms.wallet.CardRequirements;
-import com.google.android.gms.wallet.IsReadyToPayRequest;
 import com.google.android.gms.wallet.PaymentData;
 import com.google.android.gms.wallet.PaymentDataRequest;
 import com.google.android.gms.wallet.PaymentMethodTokenizationParameters;
@@ -19,9 +20,12 @@ import com.google.android.gms.wallet.PaymentsClient;
 import com.google.android.gms.wallet.TransactionInfo;
 import com.google.android.gms.wallet.Wallet;
 import com.google.android.gms.wallet.WalletConstants;
+import com.google.android.gms.wallet.IsReadyToPayRequest;
 
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -31,142 +35,154 @@ import java.util.Arrays;
 import java.util.List;
 
 public class GooglePay extends CordovaPlugin {
-    private final int LOAD_PAYMENT_DATA_REQUEST_CODE = 666;
-    private PaymentsClient mPaymentsClient;
+  private static final String IS_READY_TO_PAY = "is_ready_to_pay";
+  private static final String REQUEST_PAYMENT = "request_payment";
 
-    public static final List<Integer> SUPPORTED_METHODS = Arrays.asList(
-        WalletConstants.PAYMENT_METHOD_CARD,
-        WalletConstants.PAYMENT_METHOD_TOKENIZED_CARD
+  private static final int LOAD_PAYMENT_DATA_REQUEST_CODE = 42;
+
+  private PaymentsClient paymentsClient = null;
+  private CallbackContext callback;
+  private int environment;
+
+  @Override
+  private void pluginInitialize() {
+    this.paymentsClient = Wallet.getPaymentsClient(
+        this.cordova.getActivity().getApplicationContext(),
+        new Wallet.WalletOptions.Builder()
+            .setEnvironment(WalletConstants.ENVIRONMENT_TEST)
+            .build()
     );
+  }
 
-    public static final List<Integer> SUPPORTED_NETWORKS = Arrays.asList(
-        WalletConstants.CARD_NETWORK_AMEX,
-        WalletConstants.CARD_NETWORK_DISCOVER,
-        WalletConstants.CARD_NETWORK_VISA,
-        WalletConstants.CARD_NETWORK_MASTERCARD
-    );
+  @Override
+  public boolean execute(final String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+    this.callback = callbackContext;
 
-    private CallbackContext paymentCallbackContext;
-
-    @Override
-    protected void pluginInitialize() {
-        mPaymentsClient =
-            Wallet.getPaymentsClient(
-                cordova.getActivity(),
-                new Wallet.WalletOptions.Builder()
-                    .setEnvironment(WalletConstants.ENVIRONMENT_TEST)
-                    .build()
-            );
+    // These actions require the key to be already set
+    if (this.isInitialised()) {
+      this.callback.error("GPay not initialised.");
     }
 
-    @Override
-    public boolean execute(final String action, final JSONArray args, final CallbackContext callbackContext) throws JSONException {
-        cordova.getThreadPool().execute(new Runnable() {
-            @Override
-            public void run() {
-                if ("isReadyToPay".equals(action)) {
-                    isReadyToPay(callbackContext);
-                } else if ("makePayment".equals(action)) {
-                    makePayment(args, callbackContext);
-                }
+    if (action.equals(IS_READY_TO_PAY)) {
+      this.isReadyToPay();
+    } else if (action.equals(REQUEST_PAYMENT)) {
+      this.requestPayment(data.getString(0), data.getString(1));
+    } else {
+      return false;
+    }
+    return true;
+  }
+
+  @Override
+  public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    switch (requestCode) {
+      case LOAD_PAYMENT_DATA_REQUEST_CODE:
+        switch (resultCode) {
+          case Activity.RESULT_OK:
+            PaymentData paymentData = PaymentData.getFromIntent(data);
+            // You can get some data on the user's card, such as the brand and last 4 digits
+            CardInfo info = paymentData.getCardInfo();
+            // You can also pull the user address from the PaymentData object.
+            UserAddress address = paymentData.getShippingAddress();
+            // This is the raw JSON string version of your G Pay token.
+            String rawToken = paymentData.getPaymentMethodToken().getToken();
+
+            if (rawToken != null) {
+              // This chargeToken function is a call to your own server, which should then connect
+              // to Master Card's API to finish the charge.
+              this.callback.success(stripeToken.getId());
+            } else {
+              this.callback.error("An error occurred in processing payment");
             }
-        });
-        return true;
-    }
-
-    private void isReadyToPay(final CallbackContext callbackContext) {
-        IsReadyToPayRequest request =
-            IsReadyToPayRequest.newBuilder()
-                .addAllowedPaymentMethod(WalletConstants.PAYMENT_METHOD_CARD)
-                .addAllowedPaymentMethod(WalletConstants.PAYMENT_METHOD_TOKENIZED_CARD)
-                .build();
-        Task<Boolean> task = mPaymentsClient.isReadyToPay(request);
-        task.addOnCompleteListener(
-            new OnCompleteListener<Boolean>() {
-                public void onComplete(Task<Boolean> task) {
-                    try {
-                        boolean result = task.getResult(ApiException.class);
-                        Log.i("GooglePay", "Is ready to pay: " + result);
-                        callbackContext.success();
-                    } catch (ApiException exception) {
-                        callbackContext.error(exception.getMessage());
-                    }
-                }
-            }
-        );
-    }
-
-    private PaymentDataRequest createPaymentDataRequest(JSONObject data) {
-        PaymentDataRequest.Builder request = PaymentDataRequest.newBuilder()
-            .setTransactionInfo(TransactionInfo.newBuilder()
-                .setTotalPriceStatus(WalletConstants.TOTAL_PRICE_STATUS_FINAL)
-                .setTotalPrice(data.optString("price"))
-                .setCurrencyCode(data.optString("currency"))
-                .build()
-            )
-            .addAllowedPaymentMethods(SUPPORTED_METHODS)
-            .setCardRequirements(CardRequirements.newBuilder()
-                .addAllowedCardNetworks(SUPPORTED_NETWORKS)
-                .setAllowPrepaidCards(true)
-                .build()
-            );
-
-        PaymentMethodTokenizationParameters params = PaymentMethodTokenizationParameters.newBuilder()
-            .setPaymentMethodTokenizationType(
-                WalletConstants.PAYMENT_METHOD_TOKENIZATION_TYPE_PAYMENT_GATEWAY
-            )
-            .addParameter("gateway", "mpgs")
-            .addParameter("gatewayMerchantId", "TEST820124000")
-            .build();
-
-        request.setPaymentMethodTokenizationParameters(params);
-        return request.build();
-    }
-
-    private void makePayment(JSONArray data, CallbackContext callbackContext) {
-        PaymentDataRequest request = createPaymentDataRequest(data.optJSONObject(0));
-
-        if (request != null) {
-            Log.i("GooglePay", "PaymentRequest: 'request' is not null!");
-            AutoResolveHelper.resolveTask(
-                mPaymentsClient.loadPaymentData(request),
-                cordova.getActivity(),
-                LOAD_PAYMENT_DATA_REQUEST_CODE
-            );
+            break;
+          case Activity.RESULT_CANCELED:
+            this.callback.error("Payment cancelled");
+            break;
+          case AutoResolveHelper.RESULT_ERROR:
+            Status status = AutoResolveHelper.getStatusFromIntent(data);
+			 this.callback.error("Error in processing your payment");
+            // Log the status for debugging
+            // Generally there is no need to show an error to
+            // the user as the Google Payment API will do that
+            break;
+          default:
+            // Do nothing.
         }
-        paymentCallbackContext = callbackContext;
-        cordova.setActivityResultCallback(this);
+        break; // Breaks the case LOAD_PAYMENT_DATA_REQUEST_CODE
+      // Handle any other startActivityForResult calls you may have made.
+      default:
+        // Do nothing.
     }
+  }
 
-    @Override
-    public void onRestoreStateForActivityResult(Bundle state, CallbackContext callbackContext) {
-        paymentCallbackContext = callbackContext;
-    }
+  private boolean isInitialised() {
+    return this.paymentsClient == null;
+  }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch (requestCode) {
-            case LOAD_PAYMENT_DATA_REQUEST_CODE:
-                switch (resultCode) {
-                    case Activity.RESULT_OK:
-                        PaymentData paymentData = PaymentData.getFromIntent(data);
-                        String rawToken = paymentData.getPaymentMethodToken().getToken();
-                        paymentCallbackContext.success(rawToken);
-                        Log.i("GooglePay", "Payment OK");
-                        break;
-                    case Activity.RESULT_CANCELED:
-                        Log.i("GooglePay", "Payment Canceled");
-                        break;
-                    case AutoResolveHelper.RESULT_ERROR:
-                        Log.i("GooglePay", "Payment Error");
-                        Status status = AutoResolveHelper.getStatusFromIntent(data);
-                        break;
-                    default:
-                        // Do nothing.
-                }
-                break;
-            default:
-                // Do nothing.
+  private void isReadyToPay() {
+    IsReadyToPayRequest request = IsReadyToPayRequest.newBuilder()
+      .addAllowedPaymentMethod(WalletConstants.PAYMENT_METHOD_CARD)
+      .addAllowedPaymentMethod(WalletConstants.PAYMENT_METHOD_TOKENIZED_CARD)
+      .build();
+
+    Task<Boolean> task = this.paymentsClient.isReadyToPay(request);
+    final CallbackContext callbackContext = this.callback;
+    task.addOnCompleteListener(
+      new OnCompleteListener<Boolean>() {
+        public void onComplete(Task<Boolean> task) {
+          try {
+            boolean result = task.getResult(ApiException.class);
+            if (!result) callbackContext.error("Not supported");
+            else callbackContext.success();
+
+          } catch (ApiException exception) {
+            callbackContext.error(exception.getMessage());
+          }
         }
+      });
+  }
+
+  private void requestPayment (String totalPrice, String currency) {
+    PaymentDataRequest request = this.createPaymentDataRequest(totalPrice, currency);
+    Activity activity = this.cordova.getActivity();
+    if (request != null) {
+      cordova.setActivityResultCallback(this);
+      AutoResolveHelper.resolveTask(
+          this.paymentsClient.loadPaymentData(request),
+          activity,
+          LOAD_PAYMENT_DATA_REQUEST_CODE);
     }
+  }
+
+  private PaymentMethodTokenizationParameters createTokenisationParameters() {
+    return PaymentMethodTokenizationParameters.newBuilder()
+        .setPaymentMethodTokenizationType(WalletConstants.PAYMENT_METHOD_TOKENIZATION_TYPE_PAYMENT_GATEWAY)
+        .addParameter("gateway", "mpgs")
+        .addParameter("gatewayMerchantId", "TEST820124000")
+        .build();
+  }
+
+  private PaymentDataRequest createPaymentDataRequest(String totalPrice, String currency) {
+    PaymentDataRequest.Builder request =
+        PaymentDataRequest.newBuilder()
+            .setTransactionInfo(
+                TransactionInfo.newBuilder()
+                    .setTotalPriceStatus(WalletConstants.TOTAL_PRICE_STATUS_FINAL)
+                    .setTotalPrice(totalPrice)
+                    .setCurrencyCode(currency)
+                    .build())
+            .addAllowedPaymentMethod(WalletConstants.PAYMENT_METHOD_CARD)
+            .addAllowedPaymentMethod(WalletConstants.PAYMENT_METHOD_TOKENIZED_CARD)
+            .setCardRequirements(
+                CardRequirements.newBuilder()
+                    .addAllowedCardNetworks(Arrays.asList(
+                        WalletConstants.CARD_NETWORK_AMEX,
+                        WalletConstants.CARD_NETWORK_DISCOVER,
+                        WalletConstants.CARD_NETWORK_VISA,
+                        WalletConstants.CARD_NETWORK_MASTERCARD))
+                    .build());
+
+    request.setPaymentMethodTokenizationParameters(this.createTokenisationParameters());
+    return request.build();
+  }
 }
