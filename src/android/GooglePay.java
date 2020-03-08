db8ad03db31f36ef7922879bb26d0c0052dcfb0f
Srini-max <54628670+Srini-max@users.noreply.github.com>
2020-03-08 19:20:51 +0400

    Update GooglePay.java

--- src/android/GooglePay.java
+++ src/android/GooglePay.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.support.annotation.NonNull;
+import android.view.View;
 
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.Status;
